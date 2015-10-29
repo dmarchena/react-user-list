9892375d97229404e9602ebd3d5cc48c7f3e5a20
David Marchena <dvmarchena@gmail.com>
2015-10-29 19:04:41 +0100

    Changed title

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends React.Component {
   render() {
     return (
       <header id="header" className="header">
-        <h1 className="header__title">React Example</h1>
+        <h1 className="header__title">React + ES6 example</h1>
         <a className="header__link" href="https://github.com/dmarchena/react-user-list" title="View source code on GitHub">View source code on GitHub</a>
       </header>
     );
